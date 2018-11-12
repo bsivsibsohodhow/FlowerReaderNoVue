430b6d8e1dfe8660e1d5b0d46a2cb29646cdcdfe
Teri Ke <32941345+friedforest@users.noreply.github.com>
2018-11-12 15:48:09 -0500

    add read txt function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-New Vue({
+const template1 = '<div class="col"><p>';
+const template2 = '</p></div>';
 
+$(document).ready({
+  $.get('/data/issue-1.txt', function(data) {
+    var lines = data.split('\n');
+    for (i = 0; i < lines.length; i++) {
+      $('#reader-container').append(template1 + lines[i] + template2);
+    }
+  })
 })
