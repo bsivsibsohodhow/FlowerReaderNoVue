ba7839113e69819c561396a6e30189902b6e792c
Teri Ke <32941345+friedforest@users.noreply.github.com>
2018-11-12 15:59:51 -0500

    fix unexpected token

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 const template1 = '<div class="col"><p>';
 const template2 = '</p></div>';
 
-$(document).ready({
-  jQuery.get('/data/issue-1.txt', function(data) {
+$(document).ready(function() {
+  $.get('/data/issue-1.txt', function(data) {
     var lines = data.split('\n');
     for (i = 0; i < lines.length; i++) {
       $('#reader-container').append(template1 + lines[i] + template2);
